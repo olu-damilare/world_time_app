3d38998f5925f2418b1c8141b80379ba91521011
Damilare Jolayemi <djolayemi@gmail.com>
2021-07-02 11:54:36 +0100

    added features

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({Key? key}) : super(key: key);
+
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreenAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("Choose a location"),
+        centerTitle: true,
+      ),
+      body: Text("This is the choose location screen"),
+    );
+  }
+}
