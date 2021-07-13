221d0b5f13a7d6e2c9c7d440c6791eb38cacf6ab
Damilare Jolayemi <djolayemi@gmail.com>
2021-07-13 09:29:05 +0100

    added features

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,25 +8,51 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
+  Map data = {};
   @override
   Widget build(BuildContext context) {
+   data = ModalRoute.of(context)!.settings.arguments as Map;
+   print(data);
     return Scaffold(
       body: SafeArea(
-          child: Column(
-            children: <Widget>[
-               RaisedButton.icon(
-                   onPressed: (){
-                     Navigator.pushNamed(context, '/location');
-                   },
-                  icon: Icon(
-                      Icons.edit_location,
-                    color: Colors.white,
-                  ),
-                 label: Text("Modify location"),
-                 color: Colors.blueAccent,
-                   )
-            ],
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+            child: Column(
+              children: <Widget>[
+                 RaisedButton.icon(
+                     onPressed: (){
+                       Navigator.pushNamed(context, '/location');
+                     },
+                    icon: Icon(
+                        Icons.edit_location,
+                      color: Colors.white,
+                    ),
+                   label: Text("Modify location"),
+                   color: Colors.blueAccent,
+                     ),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0
+                      )
+                    ),
+                  ]
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                    data['time'],
+                    style: TextStyle(
+                        fontSize: 40.0,
+                        letterSpacing: 2.0
+                    )
+                ),
+              ],
+            ),
           )),
     );
   }
