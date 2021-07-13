a6a423574999421e8cd5452a6f92e6eaa6953a26
Damilare Jolayemi <djolayemi@gmail.com>
2021-07-13 09:27:07 +0100

    added features

--- lib/pages/services/world_time.dart
+++ lib/pages/services/world_time.dart
@@ -1,25 +1,31 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime{
     String location;
     String time = "kolo";
     String url;
 
-
     WorldTime(this.location, this.url);
 
   Future<void> getTime() async{
-    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
-    Map data = jsonDecode(response.body);
 
-    String dateTime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
+    try {
+      Response response = await get(
+          Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+      Map data = jsonDecode(response.body);
 
-    DateTime date = DateTime.parse(dateTime);
-    print(date);
-    date = date.add(Duration(hours: int.parse(offset)));
-    time = date.toString();
+      String dateTime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+
+      DateTime date = DateTime.parse(dateTime);
+      date = date.add(Duration(hours: int.parse(offset)));
+      time = DateFormat.jm().format(date);
+    }catch(e){
+      print('caught error: $e');
+      time = "could not fetch time data";
+    }
   }
 }
 
