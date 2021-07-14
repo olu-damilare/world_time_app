162e46b358ab7b5b1adcaecc90730ae542e67b48
Damilare Jolayemi <djolayemi@gmail.com>
2021-07-15 00:22:49 +0100

    added features

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,47 +11,77 @@ class _HomeState extends State<Home> {
   Map data = {};
   @override
   Widget build(BuildContext context) {
-   data = ModalRoute.of(context)!.settings.arguments as Map;
+   data = data.isNotEmpty ? data :  ModalRoute.of(context)!.settings.arguments as Map;
    print(data);
+
+   String bgImage = data['isDaytime'] ? 'daytime.jpg' : 'night.jpg';
+   Color textColor =  Colors.white;
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-            child: Column(
-              children: <Widget>[
-                 RaisedButton.icon(
-                     onPressed: (){
-                       Navigator.pushNamed(context, '/location');
-                     },
-                    icon: Icon(
-                        Icons.edit_location,
-                      color: Colors.white,
-                    ),
-                   label: Text("Modify location"),
-                   color: Colors.blueAccent,
-                     ),
-                SizedBox(height: 20.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      data['location'],
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/$bgImage'),
+                fit: BoxFit.cover,
+              )
+            ),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+              child: Column(
+                children: <Widget>[
+                   RaisedButton.icon(
+                       onPressed: () async {
+                       dynamic result = await  Navigator.pushNamed(context, '/location');
+                       setState(() {
+                         if (result != null) {
+                           data = {
+                             'time': result['time'],
+                             'location': result['location'],
+                             'isDaytime': result['isDaytime'],
+                           };
+                         }
+                       }
+                       );
+                       },
+                      icon: Icon(
+                          Icons.edit_location,
+                        color: Colors.white,
+                      ),
+                     label: Text(
+                         "Modify location",
+                     style: TextStyle(
+                       color: Colors.white,
+                     )),
+                     color: Colors.black,
+                       ),
+                  SizedBox(height: 20.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        data['location'],
+                        style: TextStyle(
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.w800,
+                          letterSpacing: 2.0,
+                          color: textColor,
+                        )
+                      ),
+                    ]
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                      data['time'],
                       style: TextStyle(
-                        fontSize: 28.0,
-                        letterSpacing: 2.0
+                          fontSize: 35.0,
+                          letterSpacing: 2.0,
+                        color: textColor,
+                        fontWeight: FontWeight.w800,
+
                       )
-                    ),
-                  ]
-                ),
-                SizedBox(height: 20.0),
-                Text(
-                    data['time'],
-                    style: TextStyle(
-                        fontSize: 40.0,
-                        letterSpacing: 2.0
-                    )
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
           )),
     );
